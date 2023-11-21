d5d9033fe34d0ba49aa771c0d292d34327a8d1a0
tyknkd <78797823+tyknkd@users.noreply.github.com>
2023-11-21 12:26:03 +0900

    test: coded tests

--- uk-us-english-translator/tests/1_unit-tests.js
+++ uk-us-english-translator/tests/1_unit-tests.js
@@ -4,76 +4,108 @@ const assert = chai.assert;
 const Translator = require('../components/translator.js');
 
 suite('Unit Tests', () => {
-  // Translate Mangoes are my favorite fruit. to British English
-  // Mangoes are my <span class="highlight">favourite</span> fruit.
+  const translator = new Translator();
 
-  // Translate I ate yogurt for breakfast. to British English
-  // I ate <span class="highlight">yoghurt</span> for breakfast.
+  suite('US to UK Translation', () => {
+    const locale = 'american-to-british';
+    const testCases = [
+      {text: 'Mangoes are my favorite fruit.', 
+      translation: 'Mangoes are my <span class="highlight">favourite</span> fruit.'},
+      { text: 'I ate yogurt for breakfast.', 
+      translation: 'I ate <span class="highlight">yoghurt</span> for breakfast.'},
+      {text: "We had a party at my friend's condo.",
+      translation: `We had a party at my friend's <span class="highlight">flat</span>.`},
+      {text: 'Can you toss this in the trashcan for me?',
+      translation: 'Can you toss this in the <span class="highlight">bin</span> for me?'},
+      {text: 'The parking lot was full.',
+      translation: 'The <span class="highlight">car park</span> was full.'},
+      {text: 'Like a high tech Rube Goldberg machine.',
+      translation: 'Like a high tech <span class="highlight">Heath Robinson</span> device.'},
+      {text: 'To play hooky means to skip class or work.',
+      translation: 'To <span class="highlight">bunk off</span> means to skip class or work.'},
+      {text: 'No Mr. Bond, I expect you to die.',
+      translation: 'No <span class="highlight">Mr</span> Bond, I expect you to die.'},
+      {text: 'Dr. Grosh will see you now.',
+      translation: '<span class="highlight">Dr</span> Grosh will see you now.'},
+      {text: 'Lunch is at 12:15 today.',
+      translation: 'Lunch is at <span class="highlight">12.15</span> today.'}
+    ];
 
-  // Translate We had a party at my friend's condo. to British English
-  // We had a party at my friend's <span class="highlight">flat</span>.
+    for (const testCase of testCases) {
+      test(testCase.text, () => {
+        const input = { text: testCase.text, locale: locale };
+        const result = translator.translate(input);
+        assert.property(result, 'text');
+        assert.equal(result.text, testCase.text);
+        assert.property(result, 'translation');
+        assert.equal(result.translation, testCase.translation);
+      });
+    }
+  });
 
-  // Translate Can you toss this in the trashcan for me? to British English
-  // Can you toss this in the <span class="highlight">bin</span> for me?
+  suite('UK to US Translation', () => {
+    const locale = 'british-to-american';
+    const testCases = [
+      {text: 'We watched the footie match for a while.',
+      translation: 'We watched the <span class="highlight">soccer</span> match for a while.'},
+      {text: 'Paracetamol takes up to an hour to work.',
+      translation: '<span class="highlight">Tylenol</span> takes up to an hour to work.'},
+      {text: 'First, caramelise the onions.',
+      translation: 'First, <span class="highlight">caramelize</span> the onions.'},
+      {text: 'I spent the bank holiday at the funfair.',
+      translation: 'I spent the <span class="highlight">public holiday</span> at the <span class="highlight">carnival</span>.'},
+      {text: 'I had a bicky then went to the chippy.',
+      translation: 'I had a <span class="highlight">cookie</span> then went to the <span class="highlight">fish-and-chip shop</span>.'},
+      {text: "I've just got bits and bobs in my bum bag.",
+      translation: `I've just got <span class="highlight">odds and ends</span> in my <span class="highlight">fanny pack</span>.`},
+      {text: 'The car boot sale at Boxted Airfield was called off.',
+      translation: 'The <span class="highlight">swap meet</span> at Boxted Airfield was called off.'},
+      {text: 'Have you met Mrs Kalyani?',
+      translation: 'Have you met <span class="highlight">Mrs.</span> Kalyani?'},
+      {text: "Prof Joyner of King's College, London.",
+      translation: `<span class="highlight">Prof.</span> Joyner of King's College, London.`},
+      {text: 'Tea time is usually around 4 or 4.30.',
+      translation: 'Tea time is usually around 4 or <span class="highlight">4:30</span>.'}
+    ];
+    
+    for (const testCase of testCases) {
+      test(testCase.text, () => {
+        const input = { text: testCase.text, locale: locale };
+        const result = translator.translate(input);
+        assert.property(result, 'text');
+        assert.equal(result.text, testCase.text);
+        assert.property(result, 'translation');
+        assert.equal(result.translation, testCase.translation);
+      });
+    }
+  });
 
-  // Translate The parking lot was full. to British English
-  // The <span class="highlight">car park</span> was full.
+  suite('Highlight Translation Tests', () => {
+    const locale = 'american-to-british';
+    const testCases = [
+      {text: 'Mangoes are my favorite fruit.',
+      locale: 'american-to-british', 
+      translation: 'Mangoes are my <span class="highlight">favourite</span> fruit.'},
+      { text: 'I ate yogurt for breakfast.',
+      locale: 'american-to-british',        
+      translation: 'I ate <span class="highlight">yoghurt</span> for breakfast.'},
+      {text: 'We watched the footie match for a while.',
+      locale: 'british-to-american',
+      translation: 'We watched the <span class="highlight">soccer</span> match for a while.'},
+      {text: 'Paracetamol takes up to an hour to work.',
+      locale: 'british-to-american',
+      translation: '<span class="highlight">Tylenol</span> takes up to an hour to work.'}
+    ];
 
-  // Translate Like a high tech Rube Goldberg machine. to British English
-  // Like a high tech <span class="highlight">Heath Robinson</span> device.
-
-  // Translate To play hooky means to skip class or work. to British English
-  // To <span class="highlight">bunk off</span> means to skip class or work.
-
-  // Translate No Mr. Bond, I expect you to die. to British English
-  // No <span class="highlight">Mr</span> Bond, I expect you to die.
-
-  // Translate Dr. Grosh will see you now. to British English
-  // <span class="highlight">Dr</span> Grosh will see you now.
-
-  // Translate Lunch is at 12:15 today. to British English
-  // Lunch is at <span class="highlight">12.15</span> today.
-
-  // Translate We watched the footie match for a while. to American English
-  // We watched the <span class="highlight">soccer</span> match for a while.
-
-  // Translate Paracetamol takes up to an hour to work. to American English
-  // <span class="highlight">Tylenol</span> takes up to an hour to work.
-
-  // Translate First, caramelise the onions. to American English
-  // First, <span class="highlight">caramelize</span> the onions.
-
-  // Translate I spent the bank holiday at the funfair. to American English
-  // I spent the <span class="highlight">public holiday</span> at the <span class="highlight">carnival</span>.
-
-  // Translate I had a bicky then went to the chippy. to American English
-  // I had a <span class="highlight">cookie</span> then went to the <span class="highlight">fish-and-chip shop</span>.
-
-  // Translate I've just got bits and bobs in my bum bag. to American English
-  // I've just got <span class="highlight">odds and ends</span> in my <span class="highlight">fanny pack</span>.
-
-  // Translate The car boot sale at Boxted Airfield was called off. to American English
-  // The <span class="highlight">swap meet</span> at Boxted Airfield was called off.
-
-  // Translate Have you met Mrs Kalyani? to American English
-  // Have you met <span class="highlight">Mrs.</span> Kalyani?
-
-  // Translate Prof Joyner of King's College, London. to American English
-  // <span class="highlight">Prof.</span> Joyner of King's College, London.
-
-  // Translate Tea time is usually around 4 or 4.30. to American English
-  // Tea time is usually around 4 or <span class="highlight">4:30</span>.
-
-  // Highlight translation in Mangoes are my favorite fruit.
-  // Mangoes are my <span class="highlight">favourite</span> fruit.
-
-  // Highlight translation in I ate yogurt for breakfast.
-  // I ate <span class="highlight">yoghurt</span> for breakfast.
-
-  // Highlight translation in We watched the footie match for a while.
-  // We watched the <span class="highlight">soccer</span> match for a while.
-
-  // Highlight translation in Paracetamol takes up to an hour to work.
-  // <span class="highlight">Tylenol</span> takes up to an hour to work.
-
+    for (const testCase of testCases) {
+      test(testCase.text, () => {
+        const input = { text: testCase.text, locale: testCase.locale };
+        const result = translator.translate(input);
+        assert.property(result, 'text');
+        assert.equal(result.text, testCase.text);
+        assert.property(result, 'translation');
+        assert.equal(result.translation, testCase.translation);
+      });
+    }
+  });
 });
