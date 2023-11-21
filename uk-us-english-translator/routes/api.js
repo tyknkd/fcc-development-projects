c86bf419dc7ddbdc40210947123ba7237dcf6f97
tyknkd <78797823+tyknkd@users.noreply.github.com>
2023-11-21 20:33:40 +0900

    feat: translate api

--- uk-us-english-translator/routes/api.js
+++ uk-us-english-translator/routes/api.js
@@ -4,10 +4,11 @@ const Translator = require('../components/translator.js');
 
 module.exports = function(app) {
 
-  // const translator = new Translator();
+  const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-
+      const { text, locale } = req.body;
+      res.json(translator.translate({text: text, locale: locale}));
     });
 };
